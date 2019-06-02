0e3dafb2b0b6924f4a1f693b26fd9d142e243093
jazmin.ferreiro <jazmin.ferreiro@despegar.com>
2019-06-02 18:49:30 -0300

    adding db script

---
+++ create_dev_and_test_dbs.sql
@@ -0,0 +1,7 @@
+CREATE ROLE astapor WITH LOGIN PASSWORD 'astapor'  CREATEDB;
+
+CREATE DATABASE astapor_development;
+CREATE DATABASE astapor_test;
+
+GRANT ALL PRIVILEGES ON DATABASE "astapor_development" to astapor;
+GRANT ALL PRIVILEGES ON DATABASE "astapor_test" to astapor;
