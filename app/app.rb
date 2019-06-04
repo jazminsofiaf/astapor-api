d0e68d253505464eb3784d58e4a13263f7293a5e
jazmin.ferreiro <jazmin.ferreiro@despegar.com>
2019-06-04 19:45:35 -0300

    adding controller

--- app/app.rb
+++ app/app.rb
@@ -6,18 +6,12 @@ require_relative '../models/course'
 module AstaporGuarani
   # comment
   class App < Padrino::Application
+    register Padrino::Rendering
+    register Padrino::Helpers
+
     get '/welcome_message' do
       'Hola desde Guarani'
     end
 
-    get '/' do
-      course_param = { id: 1, code: 7514, subject: 'Algebra',
-                       teacher: 'Sirne', quota: 50, modality: 'tp' }
-      algebra = Course.new(course_param)
-      CoursesRepository.new.save(algebra)
-      course = CoursesRepository.new.search_by_subject('Algebra')
-      content_type :json
-      course.to_json
-    end
   end
 end
