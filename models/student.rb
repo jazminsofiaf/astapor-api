afa51dba5db7cde1564bab47a574e7ab0c711887
jazmin.ferreiro <jazmin.ferreiro@despegar.com>
2019-06-06 01:37:17 -0300

    usuario

---
+++ models/student.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# Student class
+class Student
+  attr_accessor :id, :name, :user_name
+
+  def initialize(_id, _name, _user_name)
+    self.id = @id
+    self.name = @name
+    self.user_name = @user_name
+  end
+end
