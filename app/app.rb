7fe79d54789e0e5dde72e1b5fb66cd957084979c
Mariano <marianomartin806@gmail.com>
2019-06-06 15:49:01 -0300

    am4 case working

--- app/app.rb
+++ app/app.rb
@@ -2,6 +2,9 @@
 
 require_relative '../app/repositories/courses_repository'
 require_relative '../models/course'
+require_relative '../app/exceptions/incompatible_request_exception'
+require_relative '../app/exceptions/duplicate_subject_exception'
+require 'byebug'
 
 module AstaporGuarani
   # comment
@@ -11,6 +14,26 @@ module AstaporGuarani
 
     get '/welcome_message' do
       'Hola desde Guarani'
+    end
+
+    post '/materias' do
+      begin
+        course = CourseFromJson.parse(request.body.read)
+      rescue IncompatibleRequestException
+        status 400
+        { 'si,': '', 'no,': 'pedidos_incompatibles' }.to_json
+      end
+
+      begin
+        CoursesRepository.new.save(course)
+      rescue DuplicateSubjectException
+        status 400
+        { 'error': 'MATERIA_DUPLICADA' }.to_json
+      end
+    end
+
+    post '/reset' do
+      CoursesRepository.new.delete_all
     end
   end
 end
