47b8766a7430cbf8d5a5ea4eab1bcc32af787304
Ramiro <ram.santos@hotmail.com>
2019-06-10 01:07:58 -0300

    issue oferta academica sin filtro terminada

--- app/controllers/controllers.rb
+++ app/controllers/controllers.rb
@@ -1,3 +1,5 @@
+require_relative '../../models/courses_offers_parser'
+
 AstaporGuarani::App.controllers do
   # method for testing walking skeleton
   set :show_exceptions, false
@@ -9,5 +11,11 @@ AstaporGuarani::App.controllers do
     course = CoursesRepository.new.search_by_subject('Analisis')
     content_type :json
     course.to_json
+  end
+
+  get '/materias' do
+    courses = CoursesRepository.new.load_dataset
+    courses_response = CoursesOffersParser.new.parse(courses)
+    { 'oferta': courses_response }.to_json
   end
 end
