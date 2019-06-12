b6742d7c2a1af67606d0d94eed7aaa31a9dc96bc
Mariano Martin <marianomartin806@gmail.com>
2019-06-12 16:37:19 -0300

    New acceptance test

--- spec/models/grades_calculator_spec.rb
+++ spec/models/grades_calculator_spec.rb
@@ -7,10 +7,10 @@ require_relative '../../app/helpers/grade_helper'
 
 describe 'GradesCalculator' do
   grades_calculator = GradesCalculator.new
-  params = { name: 'Mariano Martin', user_name: 'mar2' }
-  student = Student.new(params)
 
   describe 'Calculation with exams modality' do
+    params = { name: 'Mariano Martin', user_name: 'mar2' }
+    student = Student.new(params)
     course_param = { id: 1, code: 9532, subject: 'Memo',
                      teacher: 'villagra', quota: 1, modality: 'parciales' }
     subject = Course.new(course_param)
@@ -24,6 +24,25 @@ describe 'GradesCalculator' do
 
       expect(result['status']).to eq 'APROBADO'
       expect(result['final_grade']).to eq 8
+    end
+  end
+
+  describe 'Calculation with exams modality' do
+    params = { name: 'A A', user_name: 'AA' }
+    student = Student.new(params)
+    course_param = { id: 1, code: 9532, subject: 'Memo',
+                     teacher: 'villagra', quota: 1, modality: 'parciales' }
+    subject = Course.new(course_param)
+    student.inscribe_to(subject)
+    grade = GradeHelper.new('codigo_materia' => '9532', 'notas' => '[5,5]',
+                            'username_alumno' => 'mar2')
+    student.add_grade(grade)
+
+    it 'mean must be greater than 6 in order to pass' do
+      result = grades_calculator.calculate_final_grade(student, subject)
+
+      expect(result['status']).to eq 'DESAPROBADO'
+      expect(result['final_grade']).to eq 5
     end
   end
 end
