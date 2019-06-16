e1794793c829723aee6dbddf3a9b25cc131cdd73
jazmin.ferreiro <jazmin.ferreiro@despegar.com>
2019-06-16 00:25:43 -0300

    adding available quota

--- app/helpers/courses_offers_parser.rb
+++ app/helpers/courses_offers_parser.rb
@@ -4,8 +4,10 @@ class CoursesOffersParser
   def parse(courses)
     info_courses = []
     courses.each do |course|
+      available_quota = course.quota - course.students
       info_courses.push('nombre': course.subject, 'codigo': course.code,
                         'docente': course.teacher, 'cupo': course.quota,
+                        'cupo_disponible': available_quota,
                         'modalidad': course.modality)
     end
     info_courses
